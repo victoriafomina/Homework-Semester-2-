493b9d6ea0e742b56f7b70903e8cfb199affb575
Victoria <viky.fomina@yandex.ru>
2019-02-16 21:54:36 +0300

    i am trying to finish user interface

---
+++ SortColumnsOfMatrixByFirstElements/SortColumnsOfMatrixByFirstElements/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SortColumnsOfMatrixByFirstElements
+{
+    class Sort
+    {
+        static void ShellSort(int[] array)
+        {
+            int j = 0;
+            int step = array.Length / 2;
+            while (step > 0)
+            {
+                for (int i = 0; i < array.Length - step; ++i)
+                {
+                    j = i;
+                    while (j >= 0 && array[j] > array[j + step])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + step];
+                        array[j + step] = temp;
+                        j -= step;
+                    }
+                }
+                step = step / 2;
+            }
+        }
+    }
+}
---
+++ SortColumnsOfMatrixByFirstElements/SortColumnsOfMatrixByFirstElements/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SortColumnsOfMatrixByFirstElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
