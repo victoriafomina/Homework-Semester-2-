acb78494cf6f1a11d6d1dbb4bbc63e825cf4432e
Victoria <viky.fomina@yandex.ru>
2019-02-16 23:58:00 +0300

    trying to find bug in traversalRecursion

--- spirally_traversing_of_an_array/spirally_traversing_of_an_array/Program.cs
+++ spirally_traversing_of_an_array/spirally_traversing_of_an_array/Program.cs
@@ -1,54 +1,10 @@
-﻿using System;
-
-namespace TraversingOfAnArray
+﻿namespace TraversingOfMatrix
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-
-    class Task
-    {
-        public static void UserInterface()
-        {
-            Console.WriteLine("Input size of an array");
-            int size = int.Parse(Console.ReadLine());
-            int[,] array = new int[size, size];
-            Console.WriteLine($"Input {size * size} elements");
-            for (var i = 0; i < size; ++i)
-            {
-                for (var j = 0; j < size; ++j)
-                {
-                    array[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-        }
-
-        static void Traversing(int[,] array)
-        {
-            int count = 2;
-            int x = array.GetLength(0) + 1;
-            int y = array.GetLength(0);
-            while (true)
-            {
-                int current = 0;
-                while (current < count)
-                {
-                    Console.WriteLine($"array[x - 1, y]");
-                    --x;
-                    ++current;
-                }
-                current = 0;
-                while (current < count - 1)
-                {
-                    Console.WriteLine($"array[,]");
-                    ++y;
-                    ++current;
-                }
-            }
-         
+            Task.UserInterface();
         }
     }
 }
---
+++ spirally_traversing_of_an_array/spirally_traversing_of_an_array/Task.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TraversingOfMatrix
+{
+    class Task
+    {
+        public static void UserInterface()
+        {
+            Console.WriteLine("Input dimension of a squared matrix");
+            var size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            Console.WriteLine($"Input {size * size} elements");
+            for (var i = 0; i < size; ++i)
+            {
+                for (var j = 0; j < size; ++j)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Traversing(matrix);
+        }
+
+        static void Traversing(int[,] matrix)
+        {
+            var size = matrix.GetLength(0);
+            Console.WriteLine($"{matrix[size / 2, size / 2]}");
+            if (size > 1)
+            {
+                TraversingRecursion(matrix, 3, size / 2 - 1, size / 2);
+            }
+        }
+
+        static void TraversingRecursion(int[,] matrix, int dimension, int startRow, int startColumn)
+        {
+            if (dimension > matrix.GetLength(0))
+            {
+                return;
+            }
+            for (var column = startColumn; startColumn <= startColumn + dimension - 2; ++column)
+            {
+                Console.WriteLine($"{matrix[startRow, column]}");
+            }
+            for (var row = startRow + 1; row >= startRow - dimension + 1; --row)
+            {
+                Console.WriteLine($"{matrix[row, startColumn + 1]}");
+            }
+            for (var column = startColumn + dimension - 3; column >= startColumn - 1; --column)
+            {
+                Console.WriteLine($"{matrix[startRow - dimension + 1, column]}");
+            }
+            for (var row = startRow - dimension + 2; row <= startRow; ++row)
+            {
+                Console.WriteLine($"{matrix[row, startColumn]}");
+            }
+            TraversingRecursion(matrix, dimension + 2, startRow + 1, startColumn - 1);
+        }
+    }
+}
