34db528c26d6ad63d5e3f2eacd631fe0572fccca
Victoria <viky.fomina@yandex.ru>
2019-03-17 01:16:55 +0300

    Implemented IStack interface

--- hw2/StackBasedCalculator/StackBasedCalculator/IStack.cs
+++ hw2/StackBasedCalculator/StackBasedCalculator/IStack.cs
@@ -1,7 +1,29 @@
 ﻿namespace StackBasedCalculator
 {
-    interface IStack
+    /// <summary>
+    /// Stack is a last-in-first-out-container.
+    /// </summary>
+    interface IStack<T>
     {
+        /// <summary>
+        /// Pushes an element into a stack.
+        /// </summary>
         void Push();
+
+        /// <summary>
+        /// Pops an element from the stack.
+        /// </summary>
+        void Pop();
+
+        /// <summary>
+        /// Gets a value of the element that was pushed last into a stack.
+        /// </summary>
+        /// <returns></returns>
+        T Peek();
+
+        /// <summary>
+        /// Checks if an element is in the Stack.
+        /// </summary>
+        void Exists();
     }
 }
