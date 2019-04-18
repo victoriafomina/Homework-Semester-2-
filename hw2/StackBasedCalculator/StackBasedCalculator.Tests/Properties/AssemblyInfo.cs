df8a6837a072dd42364467cf19865ac3c2a44798
Victoria <viky.fomina@yandex.ru>
2019-04-18 22:57:24 +0300

    Maybe fixed

---
+++ hw2/StackBasedCalculator/StackBasedCalculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StackBasedCalculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StackBasedCalculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b2eca6f-b251-492f-9531-9b5fadb6f0eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ hw2/StackBasedCalculator/StackBasedCalculator/MyStackOnArray.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace StackBasedCalculator
+{
+    /// <summary>
+    /// Stack is a last-in-first-out container.
+    /// </summary>
+    public class MyStackOnArray : IStack<int>
+    {
+        private int numberOfElements;
+        private int[] arrayOfElements;
+
+        /// <summary>
+        /// Construtor without parameters for MyStackOnArray class.
+        /// </summary>
+        public MyStackOnArray()
+        {
+            arrayOfElements = new int[100];
+            numberOfElements = 0;
+        }
+
+        /// <summary>
+        /// Pushes an element into a stack.
+        /// </summary>
+        public void Push(int data)
+        {
+            if (numberOfElements == arrayOfElements.Length)
+            {
+                Resize(numberOfElements);
+            }
+            arrayOfElements[numberOfElements] = data;
+            ++numberOfElements;
+        }
+
+        /// <summary>
+        /// Pops an element from the stack.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"Thrown when stack is empty.></exception>
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException($"Stack is empty! Nothing to pop!!!\n");
+            }
+            --numberOfElements;
+        }
+
+        /// <summary>
+        /// Returns an element that is in the top.
+        /// </summary>
+        /// <returns>Last element that came into a stack.</returns>
+        /// <exception cref="InvalidOperationException"Thrown when stack is empty.></exception>
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException($"Stack is empty!!!\n");
+            }
+            return arrayOfElements[numberOfElements - 1];
+        }
+
+        /// <summary>
+        /// Checks if stack is empty or not.
+        /// </summary>
+        public bool IsEmpty()
+        {
+            return numberOfElements == 0;
+        }
+
+        private void Resize(int position)
+        {
+            if (position >= numberOfElements)
+            {
+                int[] arrayCopy = new int[numberOfElements];
+                for (int i = 0; i < numberOfElements; ++i)
+                {
+                    arrayCopy[i] = arrayOfElements[i];
+                }
+                arrayOfElements = new int[numberOfElements * 2];
+                for (int i = 0; i < numberOfElements; ++i)
+                {
+                    arrayOfElements[i] = arrayCopy[i];
+                }
+            }
+        }
+    }
+}
