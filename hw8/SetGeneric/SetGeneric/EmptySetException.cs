b0ddee983e029cfb241f843be47ea46c826f34c1
Victoria <viky.fomina@yandex.ru>
2019-05-04 14:32:55 +0300

    I've creaty EmptySetException class.

---
+++ hw8/SetGeneric/SetGeneric/EmptySetException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SetGeneric
+{
+    /// <summary>
+    /// The exception that is thrown when the Set is empty.
+    /// </summary>
+    [Serializable]
+    class EmptySetException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the EmptyListException class with the specified error message.
+        /// </summary>
+        public EmptySetException(string message) : base(message) { }
+    }
+}
--- hw8/SetGeneric/SetGeneric/SetGeneric.cs
+++ hw8/SetGeneric/SetGeneric/SetGeneric.cs
@@ -7,7 +7,7 @@ namespace SetGeneric
     /// <summary>
     /// Set is an abstract data type that can store unique values.
     /// </summary>
-    public class Set<T> : ISet<T>
+    public class Set<T> : ISet<T> where T : IComparable<T>
     {
         private Node head;
         private int count;
@@ -59,15 +59,15 @@ namespace SetGeneric
 
         private bool ContainsRecursion(Node currentNode, T item)
         {
-            if (currentNode.Item == item)
+            if (currentNode.Item.Equals(item))
             {
                 return true;
             }
-            else if (currentNode.Item < item && currentNode.RightChild == null)
+            else if (currentNode.Item.CompareTo(item) < 0 && currentNode.LeftChild == null)
             {
                 return false;
             }
-            else if (currentNode.Item > item && currentNode.LeftChild == null)
+            else if (currentNode.Item.CompareTo(item) > 0 && currentNode.RightChild == null)
             {
                 return false;
             }
@@ -82,6 +82,11 @@ namespace SetGeneric
             }
         }
 
+        /// <summary>
+        /// Adds an element to the current set and returns a value to indicate if the element was successfully added.
+        /// </summary>
+        /// <param name="item">Element to add.</param>
+        /// <returns>True if the element was successfully added.</returns>
         public bool Add(T item)
         {
             if (head == null)
@@ -90,10 +95,112 @@ namespace SetGeneric
             }
             else if (Contains(item))
             {
-                return false;
+               return false;
+            }
+            else
+            {
+                AddRecursion(head, item);
             }
             ++count;
             return true;
         }
+
+        private void AddRecursion(Node currentNode, T item)
+        {
+            if (currentNode.Item.CompareTo(item) < 0 && currentNode.LeftChild != null)
+            {
+                AddRecursion(currentNode.LeftChild, item);
+            }
+            else if (currentNode.Item.CompareTo(item) > 0 && currentNode.RightChild != null)
+            {
+                AddRecursion(currentNode.RightChild, item);
+            }
+            else
+            {
+                if (currentNode.Item.CompareTo(item) < 0)
+                {
+                    currentNode.LeftChild = new Node(item);
+                }
+                else
+                {
+                    currentNode.RightChild = new Node(item);
+                }
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+            if (!Contains(item))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void RemoveRecursion(Node currentNode, T item)
+        {
+
+        }
+
+        /// <summary>
+        /// Removes all items from the Set.
+        /// </summary>
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        /// <summary>
+        /// Copies the elements of the Set to an Array, starting at a particular Array index.
+        /// </summary>
+        /// <param name="destination">The one-dimensional Array that is the destination of the elements copied from Set. 
+        /// The Array must have zero-based indexing.</param>
+        /// <param name="copyingStartsFrom">The zero-based index in array at which copying begins.</param>
+        /// <exception cref="EmptySetException">Thrown when the Set is empty.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the index at which copying starts is 
+        /// invalid.</exception>
+        /// <exception cref="ArgumentException">Thrown when the number of the elements in the list is greater than the
+        /// available space in the array.</exception>
+        public void CopyTo(T[] destination, int copyingStartsFrom)
+        {
+            if (copyingStartsFrom < 0 || copyingStartsFrom >= count)
+            {
+                throw new ArgumentOutOfRangeException($"Invalid index \"copyingStartsFrom\" {copyingStartsFrom}!!!\n");
+            }
+            if (head == null)
+            {
+                throw new EmptySetException("The set is empty!!! Invalid operation.\n");
+            }
+            if (destination.Length < count - copyingStartsFrom)
+            {
+                throw new ArgumentException("The number of elements in the Set is greater than the available space " +
+                        "from the index to the end of destination array\n");
+            }
+            
+        }
+
+        private T Traversal(Node currentNode, T[] destination, ref int copyToPos)
+        {
+            if (currentNode.LeftChild != null)
+            {
+                return Traversal(currentNode.LeftChild, destination, ref copyToPos);
+            }
+            if (currentNode.RightChild != null)
+            {
+                return Traversal(currentNode.RightChild, destination, ref copyToPos);
+            }
+            return currentNode.Item;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the Set is read-only.
+        /// </summary>
+        public bool IsReadOnly => false;
     }
 }
