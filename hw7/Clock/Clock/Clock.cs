5ecf6926d468333a5f5da37833a3a9c859fcf6e1
Victoria <viky.fomina@yandex.ru>
2019-05-08 14:22:21 +0300

    My clock works

---
+++ hw7/Clock/Clock/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Clock : Form
+    {
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void Clock_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ClockField.Text = DateTime.Now.ToString("T");
+        }
+    }
+}
