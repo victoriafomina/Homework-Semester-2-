70c4cf90fb2959b6caf5ca53be222e8e260025d3
Victoria <viky.fomina@yandex.ru>
2019-06-08 21:57:17 +0300

    Renewed class PrefixCalculator

--- hw4/ParseTree/ParseTree/ParseTree.cs
+++ hw4/ParseTree/ParseTree/ParseTree.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace PrefixParseTree
 {
@@ -47,12 +48,44 @@ namespace PrefixParseTree
                 symbols.Enqueue(expressionInArray[i]);
             }
 
-            ParseRecursion(symbols);
+            root = ParseRecursion(symbols);
         }
         
-        private void ParseRecursion(Queue<string> expression)
+        private Node ParseRecursion(Queue<string> expression)
         {
-            
+            Node newNode;
+
+            if (expression.Dequeue() == "(")
+            {
+                switch (expression.Dequeue())
+                {
+                    case "+":
+                        newNode = new Addition();
+                        break;
+                    case "-":
+                        newNode = new Subtraction();
+                        break;
+                    case "*":
+                        newNode = new Multiplication();
+                        break;
+                    case "/":
+                        newNode = new Division();
+                        break;
+                    default:
+                        throw new FormatException("Invalid format of input string!\n");
+                }
+
+                newNode.LeftChild = ParseRecursion(expression);
+                newNode.RightChild = ParseRecursion(expression);
+
+                expression.Dequeue();
+            }
+            else
+            {
+                newNode = new Operand(int.Parse(expression.Dequeue()));
+            }
+
+            return newNode;
         }
 
         private bool IsOperator(char symbol) => symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/';
---
+++ hw4/ParseTree/ParseTree/PrefixCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace PrefixParseTree
+{
+    /// <summary>
+    /// This class lets calculate prefix expressions.
+    /// </summary>
+    public class PrefixCalculator
+    {
+        string expression;
+
+        /// <returns>a string that represents the current object.</returns>
+        public override string ToString()
+        {
+            string output = "";
+
+            output += Calculate(expression);
+
+            return output;
+        }
+
+        /// <summary>
+        /// Calculates an expression.
+        /// </summary>
+        public int Calculate(string expression)
+        {
+            this.expression = expression;
+
+            var parsing = new ParseTree(expression);
+
+            return parsing.Calculate();
+        }
+    }
+}
