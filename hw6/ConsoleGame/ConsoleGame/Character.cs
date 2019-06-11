d8e773765afbec2b952f832078d2be78ddd97e84
Victoria <viky.fomina@yandex.ru>
2019-06-11 12:52:58 +0300

    Smiley is in process

---
+++ hw6/ConsoleGame/ConsoleGame/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleGame
+{
+    /// <summary>
+    /// Represents a smiley that moves on the map.
+    /// </summary>
+    public class Smiley
+    {
+        /// <summary>
+        /// Initializes an object of the class Smiley.
+        /// </summary>
+        public Smiley(int startingTop, int staringLeft)
+        {
+            TopPosition = startingTop;
+            LeftPosition = staringLeft;
+        }
+
+        /// <summary>
+        /// Returns top position.
+        /// </summary>
+        public int TopPosition { get; private set; }
+
+        /// <summary>
+        /// Returns left position.
+        /// </summary>
+        public int LeftPosition { get; private set; }
+
+
+    }
+}
