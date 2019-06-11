f169a2c291a1af50fc12f68cef3e5964f2f4cf49
Victoria <viky.fomina@yandex.ru>
2019-06-11 13:24:36 +0300

    SmileyHadler constructor is ready

---
+++ hw6/ConsoleGame/ConsoleGame/SmileyHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleGame
+{
+    /// <summary>
+    /// Handles Smiley's actions.
+    /// </summary>
+    public class SmileyHandler
+    {
+        private Map map;
+        private Smiley smiley;
+        
+        /// <summary>
+        /// Initializes an object of the SmileyHandler class.
+        /// </summary>
+        public SmileyHandler(string path)
+        {
+            map = new Map(path);
+            smiley = new Smiley(map.StatringPosition.top, map.StatringPosition.left);
+
+            Console.CursorVisible = false;
+            Console.WriteLine("☻ likes to move it, move it!\n☻ likes to move it, move it!\n☻likes to move it!" +
+                    "Use arrows to move Smiley!");
+
+            map.Display();
+            smiley.Display();
+        }
+
+    }
+}
